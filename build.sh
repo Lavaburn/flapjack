2ac1ad6c6f16bc533edcc33bb29795bdb44dbb0c
Lavaburn <nicolas@truyens.com>
2016-01-15 17:49:30 +0100

    Updating the build file to use kingpin from github (suspect issue with gopkg.in)
    Change-Id: I5d61ecbad94dbc350a0b49dbed15c6edd71a65ef

--- build.sh
+++ build.sh
@@ -6,9 +6,17 @@ export GOPATH=$(pwd):$GOPATH
 
 go get github.com/garyburd/redigo/redis
 go get github.com/go-martini/martini
-go get gopkg.in/alecthomas/kingpin.v1
 go get github.com/oguzbilgic/pandik
 mv bin/pandik libexec/httpchecker
+
+# Stopped working between 1/jan and 15/jan 2015 !!!
+	# go get gopkg.in/alecthomas/kingpin.v1
+gopath=$(pwd)	
+go get github.com/alecthomas/kingpin
+cd $gopath/src/github.com/alecthomas/kingpin
+git checkout v1.3.7
+cd $gopath
+ln -s $gopath/src/github.com/alecthomas/kingpin $gopath/src/gopkg.in/alecthomas/kingpin.v1
 
 go test flapjack
 
